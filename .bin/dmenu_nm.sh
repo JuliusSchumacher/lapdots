503f6f6e54f92de3f061ae15dc2b3730e0a0f550
UnbatedFlunky <juliusschumacher@gmail.com>
2018-10-13 11:14:31 +0200

    changes in scripts

--- .bin/dmenu_nm.sh
+++ .bin/dmenu_nm.sh
@@ -4,4 +4,4 @@
 . "${HOME}/.cache/wal/colors.sh"
 
 
-networkmanager_dmenu -w 400 -h 10 -x 0 -y 0 -p ">>" -fn "-misc-tamsyn-medium-r-normal-*-12-*-*-*-*-iso8859-1" -sb $color2 -nb $color0 -s 1
+networkmanager_dmenu -w 400 -h 10 -x 0 -y 0 -p ">>" -fn "-misc-tamzen-medium-r-normal-*-12-*-*-*-*-iso8859-1" -sb $color2 -nb $color0 -s 1
